b4f3945e92751c6abc020033028bbee86af081f5
Manuel Ventura <nelo.manuel.790@gmail.com>
2023-07-05 00:16:10 +0100

    ...

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -5,24 +5,6 @@ import {useEffect, useState} from "react";
 
 export const Search = ()=>{
     const [search, setSearch] = useState("");
-    const [movies, setMovies] = useState([])
-
-    useEffect(()=>{
-        if(search.length >= 3){
-            fetch(`https://api.themoviedb.org/3/search/movie?query=${search}&language=pt-BR&page=1`,{
-                method: 'GET',
-                headers: {
-                    accept: 'application/json',
-                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YjQ3NmMwYWQxZDFmMDFjYTZlZThkZmU5NTRmZWM2YyIsInN1YiI6IjY0YTA5MTMyOGMwYTQ4MDBhZTI0MGJiNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.I6Ju4W89keiSIrDfwMsrxBj0dghclosb9Y--AnmT1h8'
-                }
-            })
-                .then(req=>req.json())
-                .then(res=>{
-                    console.log(res.results)
-                })
-                .catch(error=>console.log(error))
-        }
-    }, [search])
 
     return(
         <form className={`w-11/12 h-14 flex my-2 self-center ${styles.search}`} action={`/search/${search}`}>
